7e6cd8b526b7a5486f0f97a14b16d9c7bb7848e4
silvia <silvia.sg@hotmail.es>
2022-08-22 11:07:58 +0200

    exercice1

---
+++ src/components/SolutionLetters.js
@@ -0,0 +1,11 @@
+
+
+const SolutionLetters = (props)=>{
+    return (<div className='solution'>
+    <h2 className='title'>Solución:</h2>
+    <ul className='letters'>{props.userLetters}</ul>
+  </div>)
+
+}
+export default SolutionLetters;
+
